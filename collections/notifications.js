068507fc8bc4a19f6f683de2f6cf9edc62c5ada1
alex-zelikow <alex.zelikow@gmail.com>
2014-05-02 08:42:52 +0800

    Display notifications in the header

--- collections/notifications.js
+++ collections/notifications.js
@@ -1,9 +1,5 @@
 Notifications = new Meteor.Collection('notifications');
 
-ownsDocument = function(userId, doc) {
-  return doc && doc.userId === userId;
-}
-
 Notifications.allow({
   update: ownsDocument
 });
