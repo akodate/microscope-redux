ad9e1b1ef26871927c311840997347d966f03533
alex-zelikow <alex.zelikow@gmail.com>
2014-05-02 08:34:44 +0800

    Added basic notifications collection

---
+++ collections/notifications.js
@@ -0,0 +1,22 @@
+Notifications = new Meteor.Collection('notifications');
+
+ownsDocument = function(userId, doc) {
+  return doc && doc.userId === userId;
+}
+
+Notifications.allow({
+  update: ownsDocument
+});
+
+createCommentNotification = function(comment) {
+  var post = Posts.findOne(comment.postId);
+  if (comment.userId !== post.userId) {
+    Notifications.insert({
+      userId: post.userId,
+      postId: post._id,
+      commentId: comment._id,
+      commenterName: comment.author,
+      read: false
+    });
+  }
+};
--- server/publications.js
+++ server/publications.js
@@ -2,3 +2,5 @@ Meteor.publish('posts', function() { return Posts.find();
 });
 Meteor.publish('comments', function(postId) { return Comments.find({postId: postId});
 });
+Meteor.publish('notifications', function() { return Notifications.find();
+});
