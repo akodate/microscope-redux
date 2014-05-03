6909d7956eb67c36dd62a97f76921fb680a1a7dd
alex-zelikow <alex.zelikow@gmail.com>
2014-05-03 23:39:16 +0800

    Added enough posts that pagination is necessary

--- server/fixtures.js
+++ server/fixtures.js
@@ -37,6 +37,11 @@ Posts.insert({
 title: 'The Meteor Book',
 userId: tom._id,
 author: tom.profile.name,
-url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000,
-commentsCount: 0
-}); }
+url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000, commentsCount: 0
+});
+for (var i = 0; i < 10; i++) { Posts.insert({
+title: 'Test post #' + i,
+author: sacha.profile.name,
+userId: sacha._id,
+url: 'http://google.com/?q=test-' + i, submitted: now - i * 3600 * 1000, commentsCount: 0
+}); } }
