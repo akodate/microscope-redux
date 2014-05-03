80628c69f242ca6c4ff5323392e82a9174aba8bd
alex-zelikow <alex.zelikow@gmail.com>
2014-05-04 02:54:20 +0800

    Added basic upvoting algorithm

--- server/fixtures.js
+++ server/fixtures.js
@@ -12,8 +12,9 @@ title: 'Introducing Telescope',
 userId: sacha._id,
 author: sacha.profile.name,
 url: 'http://sachagreif.com/introducing-telescope/', submitted: now - 7 * 3600 * 1000,
-commentsCount: 2
-});
+    commentsCount: 2,
+    upvoters: [], votes: 0
+  });
 Comments.insert({
 postId: telescopeId,
 userId: tom._id,
@@ -30,18 +31,20 @@ Posts.insert({
 title: 'Meteor',
 userId: tom._id,
 author: tom.profile.name,
-url: 'http://meteor.com', submitted: now - 10 * 3600 * 1000,
-commentsCount: 0
-});
+url: 'http://meteor.com', submitted: now - 10 * 3600 * 1000, commentsCount: 0,
+    upvoters: [], votes: 0
+  });
 Posts.insert({
 title: 'The Meteor Book',
 userId: tom._id,
 author: tom.profile.name,
-url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000, commentsCount: 0
+url: 'http://themeteorbook.com', submitted: now - 12 * 3600 * 1000, commentsCount: 0,
+upvoters: [], votes: 0
 });
 for (var i = 0; i < 10; i++) { Posts.insert({
 title: 'Test post #' + i,
 author: sacha.profile.name,
 userId: sacha._id,
-url: 'http://google.com/?q=test-' + i, submitted: now - i * 3600 * 1000, commentsCount: 0
+url: 'http://google.com/?q=test-' + i, submitted: now - i * 3600 * 1000, commentsCount: 0,
+  upvoters: [], votes: 0
 }); } }
